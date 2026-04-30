8a1d0bd13ae00eca2501855e432987227f0bf035
nhassl3 <matvey.kvasov.05@mail.ru>
2026-05-01 00:11:03 +0300

    update all sql commands (big improvements)

--- db/query/auth.sql
+++ db/query/auth.sql
@@ -1,10 +1,25 @@
--- name: Authorize :one
-INSERT INTO users (username, full_name, phone_number) VALUES (sqlc.arg('username'), sqlc.narg('full_name')::varchar, sqlc.arg('phone_number'))
-ON CONFLICT (username) DO UPDATE SET last_login=now() WHERE username=excluded.username AND phone_number=excluded.phone_number
+-- name: CreateUser :one
+INSERT INTO users(username, full_name, phone_number)
+VALUES (sqlc.arg('username'), sqlc.narg('full_name')::varchar, sqlc.arg('phone_number'))
 RETURNING *;
 
--- name: Verify :exec
-UPDATE users SET is_verified=true, updated_at=now() where username=$1;
+-- name: GetUserByUsername :one
+SELECT * FROM users WHERE username=$1;
+
+-- name: GetUserByPhone :one
+SELECT * FROM users WHERE phone_number=$1;
+
+-- name: ExistsByUsername :one
+SELECT EXISTS(SELECT 1 FROM users WHERE username=$1) AS exists;
+
+-- name: ExistsByPhoneNumber :one
+SELECT EXISTS(SELECT 1 FROM users WHERE phone_number=$1) AS exists;
+
+-- name: VerifyUser :exec
+UPDATE users SET is_verified=true, last_login=now(), updated_at=now() WHERE username = $1;
+
+-- name: UpdateLastLogin :exec
+UPDATE users SET last_login=now(), updated_at=now() WHERE username = $1;
 
 -- name: ChangePhoneNumber :one
-UPDATE users SET phone_number=$1, updated_at=now() where username=$2 RETURNING *;
+UPDATE users SET phone_number = $1, updated_at=now() WHERE username=$2 RETURNING *;
