00cbc337c0765e7ea736eb1ec3c08390048710d7
nhassl3 <matvey.kvasov.05@mail.ru>
2026-07-20 13:27:55 +0300

    feat: add parameters for update booking status, refactor: remove status for creating booking record on request

--- internal/domain/booking.go
+++ internal/domain/booking.go
@@ -38,7 +38,6 @@ type CreateBookingRequest struct {
 	StartsAt      time.Time
 	EndsAt        time.Time
 	Description   string
-	Status        BookingStatus
 }
 
 type GetBookingRequest struct {
@@ -65,7 +64,90 @@ type Booking struct {
 
 type BookingRepository interface {
 	CreateBooking(ctx context.Context, params *CreateBookingRequest) (*Booking, error)
-	GetBooking(ctx context.Context, params *GetBookingRequest) ([]*Booking, error)
+	GetBookings(ctx context.Context, params *GetBookingRequest) ([]*Booking, error)
+	UpdateBookingStatus(ctx context.Context, params *UpdateBookingStatusRequest) (*Booking, error)
+}
+
+type FindByUsername struct {
+	Username string
+	StartsAt time.Time
+}
+
+type FindByHairdresser struct {
+	HairdresserUID string
+	StartsAt       time.Time
+}
+
+type FindByService struct {
+	ServiceID int32
+	StartsAt  time.Time
+}
+
+type FindBySalon struct {
+	SalonID  int32
+	StartsAt time.Time
+}
+
+type UpdateBookingStatusRequest struct {
+	ID                *int64
+	FindByUsername    *FindByUsername
+	FindByHairdresser *FindByHairdresser
+	FindByService     *FindByService
+	FindBySalon       *FindBySalon
+	Status            BookingStatus
+}
+
+func NewUpdateBookingStatusRequest(status BookingStatus) *UpdateBookingStatusRequest {
+	return &UpdateBookingStatusRequest{
+		Status: status,
+	}
+}
+
+func (upd *UpdateBookingStatusRequest) WithID(ID *int64) *UpdateBookingStatusRequest {
+	return &UpdateBookingStatusRequest{
+		ID:     ID,
+		Status: upd.Status,
+	}
+}
+
+func (upd *UpdateBookingStatusRequest) WithUsername(username string, startsAt time.Time) *UpdateBookingStatusRequest {
+	return &UpdateBookingStatusRequest{
+		FindByUsername: &FindByUsername{
+			Username: username,
+			StartsAt: startsAt,
+		},
+		Status: upd.Status,
+	}
+}
+
+func (upd *UpdateBookingStatusRequest) WithService(serviceID int32, startsAt time.Time) *UpdateBookingStatusRequest {
+	return &UpdateBookingStatusRequest{
+		FindByService: &FindByService{
+			ServiceID: serviceID,
+			StartsAt:  startsAt,
+		},
+		Status: upd.Status,
+	}
+}
+
+func (upd *UpdateBookingStatusRequest) WithSalon(salonID int32, startsAt time.Time) *UpdateBookingStatusRequest {
+	return &UpdateBookingStatusRequest{
+		FindBySalon: &FindBySalon{
+			SalonID:  salonID,
+			StartsAt: startsAt,
+		},
+		Status: upd.Status,
+	}
+}
+
+func (upd *UpdateBookingStatusRequest) WithHairdresser(hairdresserUID string, startsAt time.Time) *UpdateBookingStatusRequest {
+	return &UpdateBookingStatusRequest{
+		FindByHairdresser: &FindByHairdresser{
+			HairdresserUID: hairdresserUID,
+			StartsAt:       startsAt,
+		},
+		Status: upd.Status,
+	}
 }
 
 // TODO: implement marshall and unmarshall JSON code for redis with encoding/json package
